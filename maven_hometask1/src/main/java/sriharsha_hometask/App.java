a983efa307c53dddc5574c103a486b3c9fb57355
Mudigonda Sri Harsha <sonusri.harsha143@gmail.com>
2020-02-02 21:52:45 +0530

    first commit

---
+++ maven_hometask1/src/main/java/sriharsha_hometask/App.java
@@ -0,0 +1,13 @@
+package sriharsha_hometask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
